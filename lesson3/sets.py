a9efee2fb1385b69ed5600b8ee6e7bbba3ee9a09
Diana Hernandez <diher@mit.edu>
2020-07-24 13:45:55 -0400

    addingin in sets tutorial

---
+++ lesson3/sets.py
@@ -0,0 +1,21 @@
+# sets are exactlt what they sound like mathematically
+# to initialize, there are two options
+empty_set = set() #for an empty set
+empty_set.add("adfad")
+initialized_set = {"a","b","c"}
+init_with_set = set("adfadfsa")
+this_is_a_dictionary = {}
+# so don't initialize an empty set with brackets
+print(empty_set, initialized_set, init_with_set)
+
+# what did you nmotice about init_with_set?
+set1 = set("adfadfasdfads")
+set2 = set("adfnvve")
+print(set1, set2)
+
+# we can do more!
+print("------------------set theory--------------")
+print(set1 - set2) # complement
+print(set2 - set1) # complement
+print(set1 | set2) # union
+print(set1 & set2) # intersection

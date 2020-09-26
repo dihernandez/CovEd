48ff7e2964a0a38abd3b49bec0262b025eb1e870
Diana Hernandez <diher@mit.edu>
2020-09-26 12:35:41 -0400

    added complete_main

---
+++ CPP/lesson1/complete_main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void func1(){
+    cout << "This is the most basic function" << endl;
+}
+
+int main(int argc, char **argv) {
+
+    cout << "This is the most complete form of main" << endl;
+
+    cout << "argc is the count of number of arguments passed in through the command line " << argc << endl;
+
+    cout << "argv is the list of arguments passed in. Argument 0 is the program name. This is an addres and the following is argv " << argv << endl;
+
+    for(int i = 0; i < argc; i++) {
+        cout << "argv[i] is " <<  argv[i] << endl;
+    }
+
+    return 0;
+}
